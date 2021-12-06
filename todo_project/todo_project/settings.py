6f10c572156d4973ad4bc6bcea4f52b39ce91201
Alex <a.a.alexashkin@mail.ru>
2021-12-06 18:34:38 +0300

    Lesson_12_other

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -87,12 +87,25 @@ WSGI_APPLICATION = 'todo_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+POSTGRES = True
+if POSTGRES:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'todo_project',
+            'USER': 'alex',
+            'PASSWORD': 'q1wertyu',
+            'HOST': 'db',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
